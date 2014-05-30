79aca07c90a4c1671efcd2fee43fadc37d2de9e5
Kyrylo Perevozchykov <kperevozchikov@cybervisiontech.com>
2014-05-30 20:07:19 +0300

    add custom slider, change buttons

--- apps/common/static/js/room_detail.js
+++ apps/common/static/js/room_detail.js
@@ -11,8 +11,20 @@ function togglePlayPause(button) {
     }
 }
 
-function setVolume(volume_range) {
-    var volume = volume_range;
-    var audio = volume_range.parentNode.parentNode.querySelector("audio")
-    audio.volume = volume.value;
+function setVolume(volume_node, value) {
+    var audio = volume_node.parentNode.parentNode.parentNode.querySelector("audio")
+    audio.volume = value;
 }
+
+$(document).ready(function() {
+    $("input[type=range]").slider({
+        min: 0,
+        max: 1,
+        step: 0.01,
+        value: 1,
+        tooltip: "hide",
+    });
+    $("input[type=range]").on("slide", function(slideEvent) {
+        setVolume(this, slideEvent.value);
+    });
+})
