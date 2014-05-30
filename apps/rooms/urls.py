f53350a5521931e56043498c0892f33a61254ef7
Sasha Myasoedov <msoedov@gmail.com>
2014-05-30 23:34:36 +0300

    delete view

--- apps/rooms/urls.py
+++ apps/rooms/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns(
     'rooms.views',
     url(r'^(?P<shorten_id>\S{4,8})/signalling$', 'signaling', name='signaling'),
     url(r'^(?P<shorten_id>\S{4,8})$', 'room_detail', name='detail'),
+    url(r'^delete/(?P<shorten_id>\S{4,8})$', 'room_delete', name='delete'),
     url(r'^$', 'room_create', name='create')
 )
--- apps/rooms/views.py
+++ apps/rooms/views.py
@@ -1,6 +1,7 @@
 import json
-from django.views.generic import CreateView, DetailView, View
+from django.views.generic import CreateView, DetailView, View, DeleteView
 from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 import django_rq
 
@@ -37,6 +38,26 @@ class SignalingUpdate(View):
     post = options = get
 
 
+class RoomDeleteView(DeleteView):
+    model = Room
+
+    def get_object(self):
+        decoded_id = short_url.decode_url(self.kwargs.get('shorten_id'))
+        return get_object_or_404(self.model, **{'id': decoded_id})
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        room = self.kwargs.get('shorten_id')
+        packet = ({}, 'deleted')
+        redis_client.publish(room, json.dumps(packet))
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('rooms:create')
+
+    get = delete
+
 room_create = RoomCreateView.as_view()
 room_detail = RoomDetailView.as_view()
 signaling = SignalingUpdate.as_view()
+room_delete = RoomDeleteView.as_view()
