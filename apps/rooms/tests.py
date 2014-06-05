0d95afb7aac3093d8f6184ba3d27f516472bfbd5
Sasha Myasoedov <msoedov@gmail.com>
2014-06-05 21:22:19 +0300

    freshup tests

--- apps/rooms/tests.py
+++ apps/rooms/tests.py
@@ -20,7 +20,7 @@ class BddVoiceAppInFirefox(LiveServerTestCase):
     def test_room_creation(self):
         driver = self.driver
         driver.get(self.live_server_url)
-        self.assertIn("LoL Voice", driver.title)
+        self.assertIn("None And Void", driver.title)
 
         submit_button = driver.find_element_by_name("submit")
 
@@ -29,14 +29,14 @@ class BddVoiceAppInFirefox(LiveServerTestCase):
         actions.click(submit_button)
         actions.perform()
 
-        self.assertIn("LoL Voice", driver.title)
+        self.assertIn("None And Void", driver.title)
 
     def test_join_the_room(self):
         room = Room.objects.create()
         driver = self.driver
         driver.get("{}/rooms/{}".format(self.live_server_url, room.shorten_url))
 
-        self.assertIn("LoL Voice", driver.title)
+        self.assertIn("None And Void", driver.title)
 
     @classmethod
     def tearDownClass(cls):
