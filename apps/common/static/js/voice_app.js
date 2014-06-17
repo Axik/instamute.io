54683bf6d0216644a6b500b8eb3e877f1796cbb8
Sasha Myasoedov <msoedov@gmail.com>
2014-06-18 01:27:59 +0300

    Finaly

--- apps/common/static/js/voice_app.js
+++ apps/common/static/js/voice_app.js
@@ -22,12 +22,21 @@ function VoiceApp(room) {
 }
 
 var peer_config ={
-      {url:'stun:stun1.l.google.com:19302'},
+      url:'stun:stun1.l.google.com:19302',
       iceServers: [{
-         url: 'turn:numb.viagenie.ca',
-         credential: 'muazkh',
-         username: 'webrtc@live.com'
-
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+         },
+         {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+         },
+         {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
          }]
     };
 
