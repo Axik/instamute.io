6a36ab05bb4a4028b75997d8269530960ff2d0a2
Kyrylo Perevozchykov <kperevozchikov@cybervisiontech.com>
2014-06-17 20:44:22 +0300

    add manual wrapper for wsgi to newrelic

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 import os
+import newrelic.agent
 
+newrelic.agent.initialize('newrelic.ini')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = newrelic.agent.WSGIApplicationWrapper(application)
