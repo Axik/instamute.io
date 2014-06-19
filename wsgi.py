0da8addc6c907a9110d18f51ab7626f9982b7b34
valsic <VSichkovskiy@carbonite.com>
2014-06-20 00:51:11 +0300

    NewRelic wsgi wrapper

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,16 @@
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#Let's wrap with newrelic if it's installed
+try:
+    from newrelic import agent
+    confpath = os.environ.get('NEWRELIC_CONFIG_PATH')
+    if confpath and os.path.exists(confpath):
+        agent.initialize(confpath)
+        application = agent.WSGIApplicationWrapper(get_wsgi_application())
+    else:
+        application = get_wsgi_application()
+except ImportError:
+    application = get_wsgi_application()
