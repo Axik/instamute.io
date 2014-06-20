390f6a3097bf1b0e8528d3345daf412184358073
Sasha Myasoedov <msoedov@gmail.com>
2014-06-20 20:28:25 +0300

    Rooms

--- apps/rooms/urls.py
+++ apps/rooms/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'rooms.views',
     url(r'^(?P<shorten_id>\S{4,8})/signalling$', 'signaling', name='signaling'),
-    url(r'^(?P<shorten_id>\S{4,8})$', 'room_detail', name='detail'),
+    url(r'^rooms/(?P<shorten_id>\S{4,8})$', 'room_detail', name='detail'),
     url(r'^$', 'room_create', name='create')
 )
